32ca8de3277cace3aa426918be0f398636ea6662
mumarualfajar <kuhaaku@gmail.com>
2023-03-09 04:41:16 +0700

    add: sesi-1

---
+++ intro-fundamental-go/sesi-1/app.go
@@ -0,0 +1,85 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	//var firstName string = "John"
+	//
+	//var lastName string = "Doe"
+	//
+	//var age int = 100
+	//
+	//var isMale bool = true
+	//
+	//var fullName string = fmt.Sprintf("Hallo  perkenalkan nama saya adalah %T %T %T ", firstName, lastName, age)
+	//
+	//_, _ = fullName, isMale
+	//
+	//fmt.Printf("isMale: %t, data type: %T \n", isMale, isMale)
+	//
+	//fmt.Println(fullName)
+	//
+	//fmt.Printf("Hallo  perkenalkan nama saya adalah %s %s %d \n", firstName, lastName, age)
+	//
+	//var age int = 100
+	//
+	//var isMale bool = true
+	//
+	//address := "Jalan Sudirman"
+	//
+	//_ = address
+	//
+	//... = > ellipsis
+	//
+	//fmt.Println("age: ", age)
+	//
+	//fmt.Println("isMale: ", isMale)
+	//
+	//var smallNumber int8 = 10
+	//
+	//var bigNumber int32 = 100
+	//
+	//var randomNumber int32 = int32(smallNumber)
+	//
+	//_, _, _ = smallNumber, bigNumber, randomNumber
+	//
+	//var a int32 = 128
+	//
+	//var b int8 = int8(a)
+	//
+	//var price float32 = 200.100
+	//
+	//var totalPrice int32 = int32(price)
+	//
+	//_, _, _ = b, totalPrice, price
+	//
+	//fmt.Printf("price: %.3f \n", price)
+	//
+	//num = num + 10
+	//
+	//num += 10
+	//
+	//num = num % 10
+	//
+	//num %= 10
+	//
+	//num -= 10
+	//
+	//var city1 string = "Jakarta"
+	//
+	//var city2 string = "oraș"
+	//
+	//var r rune = 'P'
+	//
+	//_, _, _ = r, city1, city2
+	//
+	//fmt.Println("Length of city1: ", len(city1))
+	//
+	//fmt.Println("Length of city2: ", len(city2))
+	//
+	//fmt.Println(len([]rune(city2)))
+	//
+	//fmt.Println(string([]int32{74, 65}))
+}
---
+++ intro-fundamental-go/sesi-1/tugas1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "a"
+	var b int = 10
+	var c bool = true
+
+	fmt.Printf("%s \n%T \n%d \n%T \n%t \n%T", a, a, b, b, c, c)
+}
